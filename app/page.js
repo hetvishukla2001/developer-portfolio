8f6fcf36df7dfe793f8c544eb50e80dd5e3607d6
hetvishukla2001 <hetvishukla2001@gmail.com>
2025-06-24 17:48:38 -0400

    Fix: Prevent SSR crash by dynamically loading

--- app/page.js
+++ app/page.js
@@ -1,19 +1,18 @@
 'use client';
-import AboutSection from "./components/homepage/about";
-import ContactSection from "./components/homepage/contact";
+
 import dynamic from 'next/dynamic';
-import HeroSection from "./components/homepage/hero-section";
-import Projects from "./components/homepage/projects";
-import Skills from "./components/homepage/skills";
 
-const Experience = dynamic(() => import("./components/homepage/experience"), { ssr: false });
-const Education = dynamic(() => import("./components/homepage/education"), { ssr: false });
+const HeroSection = dynamic(() => import('./components/homepage/hero-section'), { ssr: false });
+const AboutSection = dynamic(() => import('./components/homepage/about'), { ssr: false });
+const ContactSection = dynamic(() => import('./components/homepage/contact'), { ssr: false });
+const Projects = dynamic(() => import('./components/homepage/projects'), { ssr: false });
+const Skills = dynamic(() => import('./components/homepage/skills'), { ssr: false });
+const Experience = dynamic(() => import('./components/homepage/experience'), { ssr: false });
+const Education = dynamic(() => import('./components/homepage/education'), { ssr: false });
 
-
-export default  function Home() {
-
+export default function Home() {
   return (
-    <div suppressHydrationWarning >
+    <div suppressHydrationWarning>
       <HeroSection />
       <AboutSection />
       <Experience />
@@ -22,5 +21,5 @@ export default  function Home() {
       <Education />
       <ContactSection />
     </div>
-  )
-};
+  );
+}
