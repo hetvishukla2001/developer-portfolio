3ebbd28d9af055fa0770db012855ed95402cc8f0
hetvishukla2001 <hetvishukla2001@gmail.com>
2025-06-24 07:35:23 -0400

    Fix SSR issues with document by using dynamic

--- app/components/homepage/education/index.jsx
+++ app/components/homepage/education/index.jsx
@@ -3,7 +3,10 @@ import { educations } from "@/utils/data/educations";
 import Image from "next/image";
 import { BsPersonWorkspace } from "react-icons/bs";
 import lottieFile from '../../../assets/lottie/study.json';
-import AnimationLottie from "../../helper/animation-lottie";
+import dynamic from "next/dynamic";
+
+const AnimationLottie = dynamic(() => import("../../helper/animation-lottie"), { ssr: false });
+
 import dynamic from 'next/dynamic';
 const GlowCard = dynamic(() => import('../../helper/glow-card'), { ssr: false });
 
