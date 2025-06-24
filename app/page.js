efdad8eba7eeb2a61b520123cef188bb4e437961
hetvishukla2001 <hetvishukla2001@gmail.com>
2025-06-24 17:40:41 -0400

    Fix: Prevent SSR crash by dynamically loading

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 'use client';
 import AboutSection from "./components/homepage/about";
 import ContactSection from "./components/homepage/contact";
-import Education from "./components/homepage/education";
-import Experience from "./components/homepage/experience";
+import dynamic from 'next/dynamic';
 import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
+
+const Experience = dynamic(() => import("./components/homepage/experience"), { ssr: false });
+const Education = dynamic(() => import("./components/homepage/education"), { ssr: false });
 
 
 export default  function Home() {
