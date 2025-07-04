f96260620f5e69849cc2252b2e2e2845e3511d09
hetvishukla2001 <hetvishukla2001@gmail.com>
2025-07-03 23:32:16 -0400

    fix:add skill

--- app/page.js
+++ app/page.js
@@ -5,13 +5,14 @@
 // import Experience from "./components/homepage/experience";
 // import HeroSection from "./components/homepage/hero-section";
 // import Projects from "./components/homepage/projects";
-// import Skills from "./components/homepage/skills";
+import Skills from "./components/homepage/skills";
 
 export const dynamic = 'force-dynamic';
 export default function Home() {
 
   return (
     <div suppressHydrationWarning >
+      <Skills />
       {/* <HeroSection />
       <AboutSection />
       <Experience />
