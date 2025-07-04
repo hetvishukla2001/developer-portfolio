c0bdc0e29b2931a0743e2f1b0d78f56e45674a5f
hetvishukla2001 <hetvishukla2001@gmail.com>
2025-07-03 23:59:20 -0400

    add : experience and education

--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
 'use client';
 import AboutSection from "./components/homepage/about";
 import ContactSection from "./components/homepage/contact";
-// import Education from "./components/homepage/education";
-// import Experience from "./components/homepage/experience";
+import Education from "./components/homepage/education";
+import Experience from "./components/homepage/experience";
 // import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
@@ -13,8 +13,10 @@ export default function Home() {
   return (
     <div suppressHydrationWarning >
       <AboutSection />
+      <Experience />
       <Skills />
       <Projects />
+      <Education />
       <ContactSection />
       {/* <HeroSection />
       <AboutSection />
