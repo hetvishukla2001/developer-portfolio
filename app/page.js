fefed38e7305607f4ab842925c53f52552989828
hetvishukla2001 <hetvishukla2001@gmail.com>
2025-07-03 23:29:30 -0400

    fix:commet it out

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
 'use client';
-import AboutSection from "./components/homepage/about";
-import ContactSection from "./components/homepage/contact";
-import Education from "./components/homepage/education";
-import Experience from "./components/homepage/experience";
-import HeroSection from "./components/homepage/hero-section";
-import Projects from "./components/homepage/projects";
-import Skills from "./components/homepage/skills";
+// import AboutSection from "./components/homepage/about";
+// import ContactSection from "./components/homepage/contact";
+// import Education from "./components/homepage/education";
+// import Experience from "./components/homepage/experience";
+// import HeroSection from "./components/homepage/hero-section";
+// import Projects from "./components/homepage/projects";
+// import Skills from "./components/homepage/skills";
 
 export const dynamic = 'force-dynamic';
 export default function Home() {
