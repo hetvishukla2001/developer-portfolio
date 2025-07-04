5c4489cf35ec155c6eeeac8db47d92a0a438211b
hetvishukla2001 <hetvishukla2001@gmail.com>
2025-07-04 00:16:21 -0400

    add education error solution

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 'use client';
 import AboutSection from "./components/homepage/about";
 import ContactSection from "./components/homepage/contact";
-// import Education from "./components/homepage/education";
+import Education from "./components/homepage/education";
 import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
@@ -17,6 +17,7 @@ export default function Home() {
       <Experience />
       <Skills />
       <Projects />
+      <Education />
       <ContactSection />
       {/* <HeroSection />
       <AboutSection />
