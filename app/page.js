411641c49cf4d68c45bdecd9304369881889c0ee
hetvishukla2001 <hetvishukla2001@gmail.com>
2025-07-04 00:04:36 -0400

    add hero section

--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
 'use client';
 import AboutSection from "./components/homepage/about";
 import ContactSection from "./components/homepage/contact";
-import Education from "./components/homepage/education";
-import Experience from "./components/homepage/experience";
-// import HeroSection from "./components/homepage/hero-section";
+// import Education from "./components/homepage/education";
+// import Experience from "./components/homepage/experience";
+import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
 
@@ -12,11 +12,10 @@ export default function Home() {
 
   return (
     <div suppressHydrationWarning >
+      <HeroSection />
       <AboutSection />
-      <Experience />
       <Skills />
       <Projects />
-      <Education />
       <ContactSection />
       {/* <HeroSection />
       <AboutSection />
