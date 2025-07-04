89086ab8303dfca5329609e2d7750d38094f16ba
hetvishukla2001 <hetvishukla2001@gmail.com>
2025-07-03 23:41:24 -0400

    fix : add project and contact

--- app/page.js
+++ app/page.js
@@ -1,10 +1,10 @@
 'use client';
 // import AboutSection from "./components/homepage/about";
-// import ContactSection from "./components/homepage/contact";
+import ContactSection from "./components/homepage/contact";
 // import Education from "./components/homepage/education";
 // import Experience from "./components/homepage/experience";
 // import HeroSection from "./components/homepage/hero-section";
-// import Projects from "./components/homepage/projects";
+import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
 
 export const dynamic = 'force-dynamic';
