f9af1d9edcb078572ca401828e7fff2bd66f45c5
Hetvi Shukla <64137184+hetvishukla2001@users.noreply.github.com>
2026-07-23 11:52:31 -0400

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -4,7 +4,6 @@ export const personalData = {
   designation: "fullstack development and automation",
   description: "My name is Hetvi Shukla. I’m a passionate Software Developer with experience in full-stack development, test automation, and cloud infrastructure. I enjoy solving real-world problems through clean, scalable code and intuitive design. With strong skills in JavaScript, Python, React, and tools like Docker, Pytest, and Kibana, I’ve built efficient web applications and internal tools in agile environments. I’m a quick learner with a love for continuous improvement, always eager to explore new technologies and take on opportunities that align with my skills and passion for impactful development.",
   email: 'hetvishukla1001@gmail.com',
-  phone: '+13653561910',
   address: 'Ontario, canada',
   github: 'https://github.com/hetvishukla2001',
   linkedIn: 'https://www.linkedin.com/in/hetvi-shukla-aa467a1ab',
